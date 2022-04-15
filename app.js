aa1dea4ceb21363fae15f4f3d6b0a353adec6905
dineshkumar <dineshkumarpokkula@gmail.com>
2022-04-15 21:18:28 +0530

    added the comments and changed the logic of login section.

--- app.js
+++ app.js
@@ -64,7 +64,12 @@ app.use(express.json());
 // now save the data fruit1.
 // fruit1.save();
 
-// respond with "hello world" when a GET request is made to the homepage
+
+// ===================== GETING ALL USERS ===================
+// 1.fetch all the users from the database through FIND method
+// 2.if any error occur throw that error
+
+// respond with array of users when a GET request is made to the home route.
 app.get('/', (req, res) => {
     Fruit.find((err, data) => {
         if (!err) {
@@ -75,45 +80,78 @@ app.get('/', (req, res) => {
     })
 })
 
+// ================= CREATING THE NEW USER ===================
+// 1.fetch the user data from the front end.
+// 2.check if the user is already there in our database through findOne method.
+// 3.if we found the user then -> send user already there.
+// 4.if we not found the user then -> we register the user into the database.
+// 5.if any error happens then throw that error.
+
 // getting the new user data from post form in createAccount page.
 app.post('/createAccount', (req, res) => {
-    // creating the new user
-    const user1 = new Fruit(
-        {
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            actionPassword: req.body.actionPassword,
-            // lastLoginDate: { type: 'Date', require: true },
-            appsData: []
-        }
-    );
-    // now saving to the database;
-    user1.save((err, doc) => {
-        if (!err) {
-            console.log(doc);
-            res.send(doc);
+    // now let check the user is already there or not.
+    Fruit.findOne({ username: req.body.username }, (err, data) => {
+        // if user is there in our data base
+        if (data) {
+            // sending that we have user
+            res.json({ status: 'user is already regesterd'})
         } else {
-            console.log(err);
+            // lets create user and save to the database.
+            // creating the new user
+            const user1 = new Fruit(
+                {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    actionPassword: req.body.actionPassword,
+                    // lastLoginDate: { type: 'Date', require: true },
+                    appsData: []
+                }
+            );
+            // now saving to the database;
+            user1.save((err, doc) => {
+                if (!err) {
+                    console.log(doc);
+                    res.json({ status: "user created successfully." })
+                } else {
+                    console.log(err);
+                }
+            });
         }
-    });
+
+        if(err) {
+            res.json({ status: err});
+        }
+    }); 
+    
 })
 
-//Login router
 
+// ==================== LOGING THE USER =================== //
+// 1.find the user from the database through the FINDONE method.
+// 2.if the user was found in the database
+//  -> check for the password if password matchs then login user.
+//  -> if password is worng then send the error msg.
+// 3.if we not found the user then send error msg.
+// 4.if any other error occur the send that error msg.
+//Login router
 app.post('/login', (req, res) => {
     // finding the user through the username.
     Fruit.findOne({ username: req.body.username }, (err, doc) => {
-        if (!err) {
-            // again checking the users password
-            if (req.body.password === doc.password) {
-                res.json({ status: 'ok '});
+        //if the user present in our database.
+        if (doc) {
+            // checking for the password
+            if(req.body.password === doc.password) {
+                res.json({ status: "user anthenticated!"})
             } else {
-                res.json({ error: 'worng password!' });
+                res.json({ status: "entered password is worng!"})
             }
         } else {
-            console.log(err);
-            res.send(err)
+            res.json({ status: "user not found"})
+        }
+        // if any error occures
+        if (err) {
+            res.json({ status: err})
         }
     })
 })
